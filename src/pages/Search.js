08ebdc2d12492167997a10b4f3ded90ae7dd06b2
codersaed <codersaed@gmail.com>
2022-03-12 23:52:44 +0600

    search

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,24 @@
 import React from "react";
 import "./Search.css";
+import MicIcon from "@material-ui/icons/Mic";
+import SearchIcon from "@material-ui/icons/Search";
+import { Button } from "@material-ui/core";
 
 const Search = () => {
   return (
-    <div>
-      <p>Search</p>
-    </div>
+    <from className="search">
+      <div className="search_input">
+        <SearchIcon />
+        <input />
+        <MicIcon />
+      </div>
+      <div className="search_button">
+        <Button type="submit" variant="outlined">
+          Google Search
+        </Button>
+        <Button variant="outlined">I am feeling lucky</Button>
+      </div>
+    </from>
   );
 };
 
