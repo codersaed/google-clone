3fba002a54d35f7dcffe5dfaed3fa71a4adabde1
codersaed <codersaed@gmail.com>
2022-03-13 00:23:30 +0600

    searchPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SearchPage from "./pages/SearchPage";
 
 const App = () => {
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/search" element={<SearchPage />} />
+        </Routes>
+      </BrowserRouter>
+      ,
     </>
   );
 };
---
+++ src/pages/SearchPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchPage = () => {
+  return (
+    <div>
+      <p>search</p>
+    </div>
+  );
+};
+
+export default SearchPage;
