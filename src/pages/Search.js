c851a6b9a28786e7f7550b0c7af54dab18fe9423
codersaed <codersaed@gmail.com>
2022-03-14 00:04:22 +0600

    searchPage

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -4,7 +4,7 @@ import MicIcon from "@material-ui/icons/Mic";
 import SearchIcon from "@material-ui/icons/Search";
 import { Button } from "@material-ui/core";
 
-const Search = () => {
+const Search = ({ hideButton }) => {
   return (
     <from className="search">
       <div className="search_input">
@@ -12,12 +12,14 @@ const Search = () => {
         <input />
         <MicIcon />
       </div>
-      <div className="search_button">
-        <Button type="submit" variant="outlined">
-          Google Search
-        </Button>
-        <Button variant="outlined">I am feeling lucky</Button>
-      </div>
+      {!hideButton && (
+        <div className="search_button">
+          <Button type="submit" variant="outlined">
+            Google Search
+          </Button>
+          <Button variant="outlined">I am feeling lucky</Button>
+        </div>
+      )}
     </from>
   );
 };
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,9 +1,63 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Search from "./Search";
+import SearchIcon from "@material-ui/icons/Search";
+import {
+  Description,
+  Image,
+  LocalOffer,
+  Room,
+  MoreVert,
+} from "@material-ui/icons";
+import "./SearchPage.css";
 
 const SearchPage = () => {
   return (
-    <div>
-      <p>search</p>
+    <div className="searchPage">
+      <div className="searchPage_header">
+        <Link to="/">
+          <img src="/images/googlelogo.png" alt="Google" />
+        </Link>
+        <div className="searchPage_headerBody">
+          <Search hideButton />
+          <div className="searchPage_options">
+            <div className="searchPage_optionsLeft">
+              <div className="searchPage_option">
+                <SearchIcon />
+                <Link to="/">All</Link>
+              </div>
+              <div className="searchPage_option">
+                <Description />
+                <Link to="/">News</Link>
+              </div>
+              <div className="searchPage_option">
+                <Image />
+                <Link to="/">Images</Link>
+              </div>
+              <div className="searchPage_option">
+                <LocalOffer />
+                <Link to="/">Shopping</Link>
+              </div>
+              <div className="searchPage_option">
+                <Room />
+                <Link to="/">Maps</Link>
+              </div>
+              <div className="searchPage_option">
+                <MoreVert />
+                <Link to="/">More</Link>
+              </div>
+            </div>
+            <div className="searchPage_optionsRight">
+              <div className="searchPage_option">
+                <Link to="/">Setting</Link>
+              </div>
+              <div className="searchPage_option">
+                <Link to="/">Tools</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
